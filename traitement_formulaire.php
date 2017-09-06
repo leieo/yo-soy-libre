d1d37ed31ada22561e99de5b9aaa29298d7357c4
leieo <leila.el.omari@gmail.com>
2017-09-07 00:21:14 +0200

    pages test formulaire

--- traitement_formulaire.php
+++ traitement_formulaire.php
@@ -1,17 +1,5 @@
 <?php
 
-    $name=$_REQUEST['name'];
-    $email=$_REQUEST['email'];
-    $message=$_REQUEST['message'];
-    if (($name=="")||($email=="")||($message==""))
-        {
-		echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-	    }
-    else{		
-	    $from="From: $name<$email>\r\nReturn-path: $email";
-        $subject="Message sent using your contact form";
-		mail("leila.el.omari@gmail.com", $subject, $message, $from);
-		echo "Email sent!";
-	    } 
+
 
 ?>
